711fadbf1c8c91f07ec1b1da3ce4b3207077f5b7
bluesun147 <bluesun147@naver.com>
2023-04-04 20:41:36 +0900

    [feat] pub/sub 위한 RedisConfig 수정

--- src/main/java/com/example/springRedis/RedisConfig.java
+++ src/main/java/com/example/springRedis/RedisConfig.java
@@ -1,10 +1,15 @@
 package com.example.springRedis;
 
+import com.example.springRedis.service.RedisSubService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -23,5 +28,28 @@ public class RedisConfig {
 //        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(ChatMessage.class));
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         return redisTemplate;
+    }
+
+    // 리스너 어댑터 설정
+    // 스프링에서 비동기의 메시지 지원하는 마지막 컴포넌트
+    // 정해진 채널로 들어올 메시지 처리 할 action 정의.
+    @Bean
+    MessageListenerAdapter messageListenerAdapter() {
+        return new MessageListenerAdapter(new RedisSubService());
+    }
+
+    // 컨테이너 설정
+    @Bean
+    RedisMessageListenerContainer redisContainer() {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory());
+        container.addMessageListener(messageListenerAdapter(), topic());
+        return container;
+    }
+
+    // redis에서 pub/sub 할 채널을 지정해줌
+    @Bean
+    ChannelTopic topic() {
+        return new ChannelTopic("topic1");
     }
 }
