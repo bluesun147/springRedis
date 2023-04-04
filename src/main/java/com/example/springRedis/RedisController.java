cf122738fa006362ae5e9e40c0db4635f5816c80
bluesun147 <bluesun147@naver.com>
2023-04-04 20:17:45 +0900

    [feat] set, get RedisValue 메소드 생성

--- src/main/java/com/example/springRedis/RedisController.java
+++ src/main/java/com/example/springRedis/RedisController.java
@@ -1,6 +1,7 @@
 package com.example.springRedis;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,5 +21,17 @@ public class RedisController {
         redisService.getRedisStringValue("context");
 
         return "success";
+    }
+
+    // get sender1
+    // "{\"sender\":\"sender1\",\"context\":\"context1\"}"
+    @PostMapping("/api/redisTest")
+    public String send(@RequestBody ChatMessage chatMessage) throws JsonProcessingException {
+        redisService.setRedisValue(chatMessage);
+
+        String key = chatMessage.getSender();
+        ChatMessage chatMessage1 = redisService.getRedisValue(key, ChatMessage.class);
+
+        return chatMessage1.getContext();
     }
 }
--- src/main/java/com/example/springRedis/RedisService.java
+++ src/main/java/com/example/springRedis/RedisService.java
@@ -1,5 +1,6 @@
 package com.example.springRedis;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -11,11 +12,9 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class RedisService {
     // 레디스는 key-value 형태
+    // get/set 위한 객체
     private final RedisTemplate<String, Object> redisTemplate;
-
-    // StringRedisTemplate extends RedisTemplate<String, String>
     private final StringRedisTemplate stringRedisTemplate;
-
     private final ObjectMapper objectMapper;
 
     public void setRedisStringValue(ChatMessage chatMessage) {
@@ -32,5 +31,24 @@ public class RedisService {
         ValueOperations<String, String> stringValueOperations = stringRedisTemplate.opsForValue();
         // key와 value 출력
         System.out.println(key + " : " + stringValueOperations.get(key));
+    }
+
+    // 직접 만든 redisTemplate 사용
+    // redisTemplate과 stringRedisTemplate는 직렬화에 차이 있음
+    // s는 문자열 특화 템플릿 제공
+    // r은 자바 객체를 redis에 저장할 때 사용
+    public void setRedisValue(ChatMessage chatMessage) throws JsonProcessingException {
+        String key = chatMessage.getSender();
+        // value에 자바 객체를 String으로 변환해 저장
+        redisTemplate.opsForValue().set(key, objectMapper.writeValueAsString(chatMessage));
+    }
+
+    // 저장된 객체 조회
+    public <T> T getRedisValue(String key, Class<T> classType) throws JsonProcessingException {
+
+        // value가 String으로 저장되었기 때문에 넘겨준 classType으로 변환
+        String redisValue = (String)redisTemplate.opsForValue().get(key);
+
+        return objectMapper.readValue(redisValue, classType);
     }
 }
