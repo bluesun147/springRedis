54f3fe7f906b323a594de905d040d6483be56fdb
bluesun147 <bluesun147@naver.com>
2023-04-04 19:47:57 +0900

    [feat] set, get RedisStringValue 생성

--- src/main/java/com/example/springRedis/RedisController.java
+++ src/main/java/com/example/springRedis/RedisController.java
@@ -2,6 +2,8 @@ package com.example.springRedis;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class RedisController {
 
     private final RedisService redisService;
+
+    @PostMapping("/api/redisStringTest")
+    public String sendString(@RequestBody ChatMessage chatMessage) {
+        redisService.setRedisStringValue(chatMessage);
+
+        redisService.getRedisStringValue("sender");
+        redisService.getRedisStringValue("context");
+
+        return "success";
+    }
 }
--- src/main/java/com/example/springRedis/RedisService.java
+++ src/main/java/com/example/springRedis/RedisService.java
@@ -1,11 +1,36 @@
 package com.example.springRedis;
 
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class RedisService {
+    // 레디스는 key-value 형태
+    private final RedisTemplate<String, Object> redisTemplate;
 
+    // StringRedisTemplate extends RedisTemplate<String, String>
+    private final StringRedisTemplate stringRedisTemplate;
+
+    private final ObjectMapper objectMapper;
+
+    public void setRedisStringValue(ChatMessage chatMessage) {
+        // public interface ValueOperations<Key, Value>
+        // key로 sender, value로 입력한 값 저장됨.
+        // get sender -> tom
+        // get context -> hello
+        ValueOperations<String, String> stringValueOperations = stringRedisTemplate.opsForValue();
+        stringValueOperations.set("sender", chatMessage.getSender());
+        stringValueOperations.set("context", chatMessage.getContext());
+    }
+
+    public void getRedisStringValue(String key) {
+        ValueOperations<String, String> stringValueOperations = stringRedisTemplate.opsForValue();
+        // key와 value 출력
+        System.out.println(key + " : " + stringValueOperations.get(key));
+    }
 }
